01a3e373f2e1aeb7e332b48f4342e2e406fe0512
paolaparraparra <paolaalejandraparrap@gmail.com>
2025-08-23 22:56:12 -0400

    accepted

---
+++ repetitions.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s; cin>>s;
+    long largoAct = 1;
+    long largomayor = 1;
+    char letras = s[0];
+    for(int i = 1; i<s.length(); i++)
+    {
+        if(s[i] == s[i-1])
+        {
+            largoAct++;
+            if(largoAct > largomayor)
+            {
+                largomayor = largoAct;
+                letras = s[i];
+            }
+        }
+        else
+        {
+            largoAct = 1;
+        }
+    }
+    cout<<largomayor<<endl;
+    return 0;
+}
